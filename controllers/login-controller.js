f1ec1b7a7a253266081ee849ea8d90ab54ba7eb1
kienhoang1811 <hoangtrongkien18112000hiie@gmail.com>
2021-07-01 10:56:02 +0700

    lab03

--- controllers/login-controller.js
+++ controllers/login-controller.js
@@ -1,19 +1,23 @@
 var session = require('./session')
 
-function render(response, webconfig, username , errorMessage){
-    response.render('login',{
-        root : webconfig,
-        logged : false,
-        username : username,
-        errorMessage : errorMessage
+function render(response, webconfig, username , errorMessage, model){
+    model.getGeneralInfo(function(generalInfo){
+        response.render('login',{
+            root : webconfig.root,
+            logged : false,
+            generalInfo : generalInfo,
+            username : username,
+            errorMessage : errorMessage
+            })
     })
+   
 }
 exports.get = function (request, response , webconfig, model){
     if (session.logged(request)){
         response.redirect(webconfig.root)
     }
     else {
-        render(response, webconfig, '',false)
+        render(response, webconfig, '',false,model)
     }
 }
 exports.post = function (request, response, webconfig, model){
